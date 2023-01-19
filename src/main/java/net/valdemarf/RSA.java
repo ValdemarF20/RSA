2df7477a4f17a6e3dd05ce7d7aaf64b1272ffa0b
ValdemarF20 <valdemarfoster.dk@gmail.com>
2023-01-19 13:56:19 +0100

    Translation

--- src/main/java/net/valdemarf/RSA.java
+++ src/main/java/net/valdemarf/RSA.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
  * Encrypts a message and decrypts it again
  */
 public class RSA {
-    private final BigInteger privateKey; // d
-    private final BigInteger publicExponent = new BigInteger("65537"); // common value (e) in practice = 2^16 + 1
+    private final BigInteger privatNøgle; // d
+    private final BigInteger offentligEksponent = new BigInteger("65537"); // common value (e) in practice = 2^16 + 1
     private static BigInteger modulus; // n
-    private final Pair<BigInteger, BigInteger> publicKey; // This variable is not used within the code, but would be used in a real example
+    private final Pair<BigInteger, BigInteger> offentligNøgle; // This variable is not used within the code, but would be used in a real example
 
     // generate an N-bit (roughly) public and private key
     RSA(BigInteger messageNum) {
@@ -30,22 +30,22 @@ public class RSA {
         }
         BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE)); // Euler totient function - used to calculate the private key
 
-        publicKey = new Pair<>(publicExponent, modulus);
-        privateKey = publicExponent.modInverse(phi); // e^(-1) % phi
+        offentligNøgle = new Pair<>(offentligEksponent, modulus);
+        privatNøgle = offentligEksponent.modInverse(phi); // e^(-1) % phi
     }
 
     /**
      * Encrypts a given BigInteger using the formula:
-     * message^(e) % n
-     * @param message The input message, converted to a BigInteger
+     * besked^(e) % n
+     * @param besked The input besked, converted to a BigInteger
      * @return The encrypted BigInteger
      */
-    public BigInteger encrypt(BigInteger message) {
-        if(message == null) {
-            System.out.println("Improper message input");
+    public BigInteger encrypt(BigInteger besked) {
+        if(besked == null) {
+            System.out.println("Improper besked input");
             return null;
         }
-        return message.modPow(publicExponent, modulus);
+        return besked.modPow(offentligEksponent, modulus);
     }
 
     /**
@@ -59,7 +59,7 @@ public class RSA {
             System.out.println("Improper encryption input");
             return null;
         }
-        return encrypted.modPow(privateKey, modulus);
+        return encrypted.modPow(privatNøgle, modulus);
     }
 
     /**
@@ -68,8 +68,8 @@ public class RSA {
      */
     public String toString() {
         String s = "";
-        s += "public  = " + publicExponent + "\n\n";
-        s += "private = " + privateKey + "\n\n";
+        s += "public  = " + offentligEksponent + "\n\n";
+        s += "private = " + privatNøgle + "\n\n";
         s += "modulus = " + modulus;
         return s;
     }
@@ -79,30 +79,31 @@ public class RSA {
      * @param args Provide no args
      */
     public static void main(String[] args) {
-        // Get message from user
-        System.out.println("Message:");
+        // Get besked from user
+        System.out.println("Besked:");
         Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
+        String besked = scanner.nextLine();
         System.out.println("\n");
 
         // Check if args are entered correctly by the user
-        if(message.isEmpty() || message.isBlank()) {
-            System.out.println("Cannot use empty string");
+        if(besked.isEmpty() || besked.isBlank()) {
+            System.out.println("Kan ikke benytte tom tekst");
+            return;
         }
 
-        // Convert the message to bytes, and then encrypt and decrypt using those bytes
-        byte[] bytes = message.getBytes();
-        BigInteger messageNum = new BigInteger(bytes);
-        RSA key = new RSA(messageNum);
-        System.out.println(key);
+        // Convert the besked to bytes, and then encrypt and decrypt using those bytes
+        byte[] bytes = besked.getBytes();
+        BigInteger beskedTal = new BigInteger(bytes);
+        RSA nøgle = new RSA(beskedTal);
+        System.out.println(nøgle);
 
-        BigInteger encrypt = key.encrypt(messageNum);
-        BigInteger decrypt = key.decrypt(encrypt);
+        BigInteger encrypt = nøgle.encrypt(beskedTal);
+        BigInteger decrypt = nøgle.decrypt(encrypt);
 
         // Output
-        System.out.println("\n" + "message = " + message + " -|- message number = " + messageNum + "\n");
-        System.out.println("encrypted message = " + (new String(encrypt.toByteArray())) + "\n");
-        System.out.println("encrypted number = " + encrypt + "\n");
-        System.out.println("decrypted message = " + (new String(decrypt.toByteArray())) + " -|- decrypted number = " + decrypt);
+        System.out.println("\n" + "besked = " + besked + " -|- besked tal = " + beskedTal + "\n");
+        System.out.println("Krypteret besked = \n" + (new String(encrypt.toByteArray())) + "\n");
+        System.out.println("Krypteret tal = \n" + encrypt + "\n");
+        System.out.println("Dekrypteret besked = " + (new String(decrypt.toByteArray())) + " -|- dekrypteret tal = " + decrypt);
     }
 }
